ac897b0b9c6352e803745350673c536549a9ef9c
ajlopez <webmaster@ajlopez.com>
2014-03-30 09:08:43 -0300

    Binary search, second test implementation

--- katas/KarateChop/chop.js
+++ katas/KarateChop/chop.js
@@ -1,9 +1,30 @@
 
 function chop(value, values) {
-    if (value == values[0])
-        return 0;
-        
-    return -1;
+	var length = values.length;
+
+	var l = length;
+	var pos = Math.floor(l / 2);
+	var delta = pos;
+
+	while (pos >= 0 && pos < length) {
+		var newvalue = values[pos];
+
+		if (value == newvalue)
+			return pos;
+
+		if (delta == 0)
+			return -1;
+
+		delta = Math.floor(delta / 2);
+		l = pos;
+
+		if (value < newvalue)
+			pos = pos - (delta ? delta : 1);
+		else
+			pos = pos + (delta ? delta : 1);
+	}
+
+	return -1;
 }
 
 module.exports = chop;
--- katas/KarateChop/test.js
+++ katas/KarateChop/test.js
@@ -6,3 +6,9 @@ exports['find in one element array'] = function (test) {
     test.equal(chop(1, [2]), -1);
 }
 
+exports['find in two element array'] = function (test) {
+    test.equal(chop(1, [1, 2]), 0);
+    test.equal(chop(2, [1, 2]), 1);
+    test.equal(chop(3, [1, 2]), -1);
+}
+
