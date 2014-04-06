0c54fbb92342082336d79db0bda5db499e3bbdf0
ajlopez <webmaster@ajlopez.com>
2014-04-06 08:55:03 -0300

    Numbers one digit

--- CodeKatas/EnglishNumerals/numerals.js
+++ CodeKatas/EnglishNumerals/numerals.js
@@ -3,6 +3,14 @@ var values = [];
 
 values[0] = 'zero';
 values[1] = 'one';
+values[2] = 'two';
+values[3] = 'three';
+values[4] = 'four';
+values[5] = 'five';
+values[6] = 'six';
+values[7] = 'seven';
+values[8] = 'eight';
+values[9] = 'nine';
 
 function nums(value) {
     return values[value];
--- CodeKatas/EnglishNumerals/test.js
+++ CodeKatas/EnglishNumerals/test.js
@@ -8,3 +8,14 @@ exports['convert zero'] = function (test) {
 exports['convert one'] = function (test) {
     test.equal(nums(1), 'one');
 };
+
+exports['first numbers one digit'] = function (test) {
+    test.equal(nums(2), 'two');
+    test.equal(nums(3), 'three');
+    test.equal(nums(4), 'four');
+    test.equal(nums(5), 'five');
+    test.equal(nums(6), 'six');
+    test.equal(nums(7), 'seven');
+    test.equal(nums(8), 'eight');
+    test.equal(nums(9), 'nine');
+};
