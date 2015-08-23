217dd51167be2baf30fbc33dea4272bd30844fe7
ajlopez <webmaster@ajlopez.com>
2015-08-23 10:21:53 -0300

    Angular update; scope inheritance sample

--- Angular/app.js
+++ Angular/app.js
@@ -3,8 +3,16 @@ var app = angular.module('myApp', []);
 
 app.controller('FirstCtrl', function ($scope) {
     $scope.model = { firstName: 'Adam', lastName: 'Genesis' };
-    
+
     $scope.clickMe = function (name) {
         alert("Hello " + name);
     };
+});
+
+app.controller('ParentCtrl', function ($scope) {
+    $scope.title = "Title from parent";
+});
+
+app.controller('ChildCtrl', function ($scope) {
+    $scope.content = "Content from child";
 });
