b1cb1399bd5a34fa9381729e2a0aaebbcf2634b3
ajlopez <webmaster@ajlopez.com>
2021-03-03 18:57:12 -0300

    Text to words

---
+++ Indexer/lib/words.js
@@ -0,0 +1,37 @@
+
+function toWords(text) {
+    const words = [];
+    
+    if (!text)
+        return words;
+        
+    text = text.toLowerCase();
+    
+    const l = text.length;
+    
+    let word = '';
+    
+    for (let k = 0; k < l; k++) {
+        const ch = text[k];
+        
+        if (isLetter(ch))
+            word += ch;
+        else if (word.length > 0) {
+            words.push(word);
+            word = '';
+        }
+    }
+    
+    if (word.length)
+        words.push(word);
+        
+    return words;
+}
+
+function isLetter(ch) {
+    return ch >= 'a' && ch <= 'z';
+}
+
+module.exports = {
+    toWords
+}
---
+++ Indexer/test/words.js
@@ -0,0 +1,59 @@
+
+const words = require('../lib/words');
+
+exports['text to words'] = function (test) {
+    const result = words.toWords('hello world');
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'hello', 'world' ]);
+}
+
+exports['text to words skipping spaces'] = function (test) {
+    const result = words.toWords('  hello   world  ');
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'hello', 'world' ]);
+}
+
+exports['text to words skipping tabs, carriage returns, and new lines'] = function (test) {
+    const result = words.toWords('\t  hello \r\n  world  ');
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'hello', 'world' ]);
+}
+
+exports['text to words skipping non-letter'] = function (test) {
+    const result = words.toWords('hello world!');
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'hello', 'world' ]);
+}
+
+exports['text to words skipping digits'] = function (test) {
+    const result = words.toWords('hello 123 world!');
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'hello', 'world' ]);
+}
+
+exports['text to words in lower case'] = function (test) {
+    const result = words.toWords('HELLO 123 WORLD!');
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'hello', 'world' ]);
+}
+
+exports['empty text'] = function (test) {
+    const result = words.toWords('');
+    
+    test.ok(result);
+    test.deepEqual(result, []);
+}
+
+exports['null text'] = function (test) {
+    const result = words.toWords(null);
+    
+    test.ok(result);
+    test.deepEqual(result, []);
+}
+
