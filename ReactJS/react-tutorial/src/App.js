c34dbecb649c6e1b1c2067b511bb1930a8972bcd
ajlopez <webmaster@ajlopez.com>
2021-03-28 13:27:17 -0300

    React tutorial sample with hardcoded table data

---
+++ ReactJS/react-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Table from './Table';
+
+function App() {
+  return (
+      <div className="container">
+      <h1>Hello React</h1>
+        <Table />
+      </div>
+  );
+}
+
+export default App;
