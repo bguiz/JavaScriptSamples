a39fb8e2d3e7cfa8e0899e9ffe441903c07ea957
bguiz <bguiz@users.noreply.github.com>
2021-04-27 17:11:08 +0800

    feat: add --subdirs to whitelist a subset of directories to traverse

--- Indexer/indexer.js
+++ Indexer/indexer.js
@@ -3,6 +3,7 @@ const path = require('path');
 const simpleargs = require('simpleargs');
 
 simpleargs.define('w','weighted',false,'Weighted Word Count', { flag: true });
+simpleargs.define('sd','subdirs','','Sub directories to include');
 
 const words = require('./lib/words');
 const files = require('./lib/files');
@@ -12,47 +13,52 @@ const args = simpleargs(process.argv.slice(2));
 
 const dirpath = args._[0];
 const extension = '.' + args._[1];
+const subdirsArg = args.subdirs;
+const hasSubdirsArg = typeof subdirsArg === 'string' && subdirsArg.length > 0;
+const subdirs = (hasSubdirsArg) ?
+    subdirsArg.split(':') :
+    [];
 
 const filesdes = [];
 const result = {};
 
-files.processFiles(dirpath, extension, null, 
+files.processFiles(dirpath, extension, null, subdirs,
     filename => {
         const filepath = path.join(dirpath, filename);
         const text = fs.readFileSync(filepath).toString();
-        
+
         const title = markdown.getTitle(text);
         const summary = markdown.getSummary(text);
         const description = markdown.getDescription(text);
         const header = markdown.getHeader(text);
         const tags = markdown.getTags(text);
         const permalink = markdown.getPermalink(text);
-        
+
         const file = { n: filename };
-        
+
         if (title)
             file.t = title;
         else if (header)
             file.t = header;
-        
+
         if (summary)
             file.d = summary;
         else if (description)
             file.d = description;
-            
+
         if (tags)
             if (file.d)
                 file.d += ' ' + tags;
             else
                 file.d = tags;
-                
+
         if (permalink)
             file.l = permalink;
-            
+
         filesdes.push(file);
-        
+
         const cwords = words.countWords(words.toWords(text), args.weighted);
-        
+
         words.collectWords(result, cwords, filesdes.length - 1);
     });
 
--- Indexer/lib/files.js
+++ Indexer/lib/files.js
@@ -2,36 +2,47 @@
 const fs = require('fs');
 const path = require('path');
 
-function processFiles(dirpath, extension, prefix, fn) {
+function processFiles(dirpath, extension, prefix, subdirs, fn) {
     const filenames = fs.readdirSync(dirpath);
     const l = filenames.length;
 
     for (let k = 0; k < l; k++) {
         const filename = filenames[k];
-        
+
         if (filename === 'node_modules')
             continue;
         if (filename.toLowerCase() === 'readme.md')
             continue;
         if (filename === '.github')
             continue;
-            
+
         const filepath = path.join(dirpath,  filenames[k]);
-        
+
         const stat = fs.lstatSync(filepath);
-        
+
         if (stat.isFile() && filename.endsWith(extension)) {
             const name = prefix ? path.join(prefix, filename) : filename;
             fn(name);
             continue;
         }
-        
+
         if (stat.isDirectory()) {
             const newprefix = prefix ? path.join(prefix, filename) : filename;
             const newdirpath = path.join(dirpath, filename);
-            processFiles(newdirpath, extension, newprefix, fn);
+            let shouldRecur;
+            if (subdirs.length > 0) {
+                shouldRecur = subdirs.filter((subdir) => (
+                    // recursively track subdir/ this is more of a brute force approach
+                    newdirpath.indexOf(subdir) >= 0
+                )).length > 0;
+            } else {
+                shouldRecur = true;
+            }
+            if (shouldRecur) {
+                processFiles(newdirpath, extension, newprefix, subdirs, fn);
+            }
         }
-    }    
+    }
 }
 
 module.exports = {
